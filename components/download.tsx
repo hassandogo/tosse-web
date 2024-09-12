a6bef3c813c991bdf6dc37b2c91c5a5e73c90e56
Hassan <hassanmahamatdogo@gmail.com>
2024-09-12 08:27:55 +0100

    Modification de responsive

--- components/download.tsx
+++ components/download.tsx
@@ -24,12 +24,12 @@ const Download = () => {
           <h4 className="text-3xl md:text-4xl lg:text-6xl font-normal leading-[3rem] md:leading-[5.625rem] md:text-left text-black">
             Téléchargez l&apos;application TOSSE maintenant
           </h4>
-          <div className="w-20 bg-[#FF9D00] h-3 mx-auto"></div>
+          <div className="w-20 bg-[#FF9D00] h-3 mx-auto md:mx-0"></div>
             <p className="text-[1.375rem] font-normal leading-[2rem] md:leading-[1.65rem] md:text-left p-3">
               Vous pouvez effectuer l&apos;achat via notre application.
             </p>
 
-          <div className="gap-[1.875rem] px-2.5 flex p-4 items-center justify-center">
+          <div className="gap-[1.875rem] px-2.5 flex p-4 items-center justify-center md:justify-start">
             <Image
               src="/telecharger_play.png"
               alt="télécharger l'application"
