2f3a853088aebc1454d331e625d4c10dac31aa8c
Hassan <hassanmahamatdogo@gmail.com>
2024-09-16 18:46:58 +0100

    Modification de la page de faq

--- app/(app)/faqs/page.tsx
+++ app/(app)/faqs/page.tsx
@@ -8,13 +8,14 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Link from "next/link";
 const Faqs = () => {
   return (
     <section>
       <Header />
       <Spacer small />
       <AppContainer>
-        <div className="justify-center flex items-center flex-col gap-4 border">
+        <div className="justify-center flex items-center flex-col gap-4">
           <div className="text-center space-y-2">
             <h1 className="text-[3.125rem] font-bold leading-[3.75rem] text-[#1c2e3d] mb-[1.5rem]">
               Questions fréquemment posées
@@ -26,19 +27,21 @@ const Faqs = () => {
           {faqs &&
             faqs.length > 0 &&
             faqs.map((faq) => (
-              <div className="w-[70%] space-y-3 py-4">
-                <h3 className=" text-[2.813rem] font-normal  leading-[3.375rem] tracking-widest text-primary text-center">
+              <div className="w-[80%] space-y-3 py-4">
+                <Spacer tooSmall />
+                <h3 className=" text-[2rem] font-normal  leading-[3.375rem] tracking-widest text-primary text-center">
                   {faq.theme.title}
                 </h3>
                 <div className="">
-                  <Accordion type="single" collapsible>
+                  <Spacer small />
+                  <Accordion type="single" collapsible className="">
                     {faq.theme.content.map((quest, index) => (
                       <AccordionItem value={`item-${index}`}>
-                        <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                      {quest.question}
+                        <AccordionTrigger className="text-2xl font-normal leading-9 text-left">
+                          {quest.question}
                         </AccordionTrigger>
-                        <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                       {quest.anwser}
+                        <AccordionContent className="text-[1rem] font-normal leading-[2rem] text-left ml-8">
+                          {quest.anwser}
                         </AccordionContent>
                       </AccordionItem>
                     ))}
@@ -46,133 +49,15 @@ const Faqs = () => {
                 </div>
               </div>
             ))}
-          {/* <div className="w-[70%] space-y-3 py-4">
-            <h3 className=" text-[2.813rem] font-normal leading-[3.375rem] tracking-widest text-primary text-center">
-              Transferts de credit
-            </h3>
-            <div className="">
-              <Accordion type="single" collapsible>
-                <AccordionItem value="item-1">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Quels réseaux sont pris en charge?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left pl-8">
-                    Nous soutenons actuellement{" "}
-                    <span className="text-[#396BB4]">Moov Africa(TIGO)</span> ,{" "}
-                    <span className="text-[#EB1C24]">Airtel</span> et{" "}
-                    <span className="text-[#00FF00]">Salam</span>. Nous essayons
-                    de prendre en charge tous les réseaux disponibles à
-                    l&apos;avenir.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Quels sont les numéros de téléphone de Tosse?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                    Moov Africa(Tigo):{" "}
-                    <span className="text-[#396BB4]">95 16 10 30</span>
-                    <br />
-                    Airtel: <span className="text-[#EB1C24]">66 46 24 28</span>
-                    <br />
-                    Pour des raisons de sécurité et de confidentialité, seul le
-                    numéro qui nous contacte peut effectuer le paiement. Nous
-                    préférons donc que vous nous contactiez avec le numéro de
-                    paiement.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Pouvez-vous fournir un exemple de message texte à envoyer?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                    Envoyer 500 à 9XXXXXXX
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-4">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Y a-t-il un avantage à utiliser ce service au sein du même
-                    réseau?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                    L&apos;avantage dans ce scénario est la rapidité et la
-                    commodité.
-                    <br />
-                    Par exemple, si vous voulez vous acheter du crédit pour
-                    500frs, vous pouvez simplement envoyer un SMS avec du
-                    contenu, &quot;1000&quot;; au lieu de suivre les étapes
-                    d&apos;achat de crédit de votre opérateur réseau.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-5">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Combien de temps faudrait-il pour recevoir du credit que
-                    vous achetez?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                    C&apos;est presque instantané lorsque les conditions du
-                    réseau sont bonnes. Nous traitons votre transaction
-                    immédiatement après réception du paiement. <br />
-                    Mais cela peut prendre jusqu&apos;à 5 minutes dans de
-                    mauvaises conditions de réseau. Nous allons réessayer
-                    jusqu&apos;à ce que ça aille. Mais si cela prend trop de
-                    temps et que nous ne sommes pas en mesure d&apos;effectuer
-                    la transaction, nous effectuerons un remboursement à 100%.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-5">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Quels pays sont pris en charge?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                    Actuellement, ce service ne fonctionne qu&apos;au Tchad mais
-                    nous espérons soutenir plus de pays le long de la ligne.
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-          </div>
-          <div className="w-[70%] space-y-3 py-4">
-            <h3 className=" text-[2.813rem] font-normal leading-[3.375rem] tracking-widest text-primary text-center">
-              Paiements
-            </h3>
-            <div className="">
-              <Accordion type="single" collapsible>
-                <AccordionItem value="item-1">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Quelles méthodes de paiements sont disponibles?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                    Tous les paiements mobile money sont pris en charge (
-                    <span className="text-[#396BB4]">
-                      Moov Africa(Tigo) Money
-                    </span>
-                    et <span className="text-[#EB1C24]">Airtel Money</span>).
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Les paiements fonctionnent-ils complètement hors ligne?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                    A fairre
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                  <AccordionTrigger className="text-3xl font-normal leading-9 text-left">
-                    Existe-t-il une politique de remboursement?
-                  </AccordionTrigger>
-                  <AccordionContent className="text-[1.563rem] font-normal leading-[2.813rem] text-left">
-                    Nous vous rembourserions 100% de votre argent si nous ne
-                    sommes pas en mesure de répondre à votre demande. Si vous
-                    souhaitez annuler la demande, nous ajouterons des frais de
-                    remboursement de 3% du montant.
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-          </div> */}
         </div>
+        <Spacer tooSmall />
+        <p className="text-[1.5rem] font-normal leading-[2rem] text-center">
+          Vous avez une autre question?{" "}
+          <Link href="/#" className="text-primary">
+            Contactez-nous
+          </Link>{" "}
+        </p>
+        <Spacer tooSmall />
       </AppContainer>
     </section>
   );
--- dossier/faqs.tsx
+++ dossier/faqs.tsx
@@ -1,58 +1,94 @@
 export const faqs = [
-    {
-      theme: {
-        title: "À propos de Tosse",
-        content: [
-          {
-            question: "Que fait Tosse?",
-            anwser:
-              "TOSSE vous offre la possibilité d'acheter du credit pour n'importe quel réseau au Tchad en utilisant votre méthode de paiement préférée, sans demander de frais.",
-          },
-          {
-            question: "Que fait Tosse?",
-            anwser:
-              "TOSSE vous offre la possibilité d'acheter du credit pour n'importe quel réseau au Tchad en utilisant votre méthode de paiement préférée, sans demander de frais.",
-          },
-          {
-            question: "Que fait Tosse?",
-            anwser:
-              "TOSSE vous offre la possibilité d'acheter du credit pour n'importe quel réseau au Tchad en utilisant votre méthode de paiement préférée, sans demander de frais.",
-          },
-        ],
-      },
+  {
+    theme: {
+      title: "À propos de Tosse",
+      content: [
+        {
+          question: "Que fait Tosse?",
+          anwser:
+            "TOSSE vous offre la possibilité d'acheter du credit pour n'importe quel réseau au Tchad en utilisant votre méthode de paiement préférée, sans demander de frais.",
+        },
+        {
+          question: "Est-ce vraiment gratuit?",
+          anwser:
+            "Oui. C'est totalement gratuit. Il n'y a pas de frais demandés pour effectuer des paiements. De plus, nous n'ajoutons pas de frais de retrait à votre paiement. Par exemple, si vous voulez du credit pour 500frs, vous paierez exactement 500frs. Nous souhaitons vous informer que votre opérateur réseau peut vous facturer des SMS pour des transactions hors ligne à moins que vous ne soyez abonné à un forfait SMS.",
+        },
+        {
+          question: "La messagerie SMS est-elle gratuite?",
+          anwser:
+            "Votre opérateur réseau peut vous facturer des SMS, sauf si vous êtes abonné à un forfait SMS.",
+        },
+        {
+          question: "Existe-t-il une application Android ou iOS?",
+          anwser:
+            "Non, il n'y en a pas. Nous en fournirions un à l'avenir. Pour l'instant, vous ne pouvez utiliser que les SMS ou le site Web.",
+        },
+        {
+          question: "Tosse vend-il aussi des données?",
+          anwser:
+            "Nous vendons des données sur demande et le processus n'est pas aussi rapide et automatique que le processus de transfert de crédit. Au cas où vous vous demandez s'il y a des frais pour cela, c'est totalement gratuit.",
+        },
+      ],
     },
-    {
-      theme: {
-        title: "Transferts de credit",
-        content: [
-          {
-            question: "Quels réseaux sont pris en charge?",
-            anwser:
-              "Nous soutenons actuellement Moov Africa(TIGO) , Airtel et Salam. Nous essayons de prendre en charge tous les réseaux disponibles à l'avenir.",
-          },
-          {
-            question: "Quels réseaux sont pris en charge?",
-            anwser:
-              "Nous soutenons actuellement Moov Africa(TIGO) , Airtel et Salam. Nous essayons de prendre en charge tous les réseaux disponibles à l'avenir.",
-          },
-        ],
-      },
+  },
+  {
+    theme: {
+      title: "Transferts de credit",
+      content: [
+        {
+          question: "Quels réseaux sont pris en charge?",
+          anwser:
+            "Nous soutenons actuellement Moov Africa(TIGO) , Airtel et Salam. Nous essayons de prendre en charge tous les réseaux disponibles à l'avenir.",
+        },
+        {
+          question: "Quels sont les numéros de téléphone de Tosse?",
+          anwser:
+            "Moov Africa: 95 16 10 30 Airtel: 66 46 24 28 Pour des raisons de sécurité et de confidentialité, seul le numéro qui nous contacte peut effectuer le paiement. Nous préférons donc que vous nous contactiez avec le numéro de paiement.",
+        },
+        {
+          question:
+            "Pouvez-vous fournir un exemple de message texte à envoyer?",
+          anwser: "Envoyer 500 à 9XXXXXXX",
+        },
+        {
+          question:
+            "Y a-t-il un avantage à utiliser ce service au sein du même réseau?",
+          anwser:
+            "L'avantage dans ce scénario est la rapidité et la commodité. Par exemple, si vous voulez vous acheter du crédit pour 500frs, vous pouvez simplement envoyer un SMS avec du contenu, '500'; au lieu de suivre les étapes d'achat de crédit de votre opérateur réseau.",
+        },
+        {
+          question:
+            "Combien de temps faudrait-il pour recevoir du credit que vous achetez?",
+          anwser:
+            "C'est presque instantané lorsque les conditions du réseau sont bonnes. Nous traitons votre transaction immédiatement après réception du paiement.Mais cela peut prendre jusqu'à 5 minutes dans de mauvaises conditions de réseau. Nous allons réessayer jusqu'à ce que ça aille. Mais si cela prend trop de temps et que nous ne sommes pas en mesure d'effectuer la transaction, nous effectuerons un remboursement à 100%.",
+        },
+        {
+          question: "Quels pays sont pris en charge?",
+          anwser:
+            "Actuellement, ce service ne fonctionne qu'au Tchad mais nous espérons soutenir plus de pays le long de la ligne.",
+        },
+      ],
     },
-    {
-      theme: {
-        title: "Paiements",
-        content: [
-          {
-            question: "Quelles méthodes de paiements sont disponibles?",
-            anwser:
-              "Tous les paiements mobile money sont pris en charge (Moov Africa(Tigo) Moneyet Airtel Money).",
-          },
-          {
-            question: "Quelles méthodes de paiements sont disponibles?",
-            anwser:
-              "Tous les paiements mobile money sont pris en charge (Moov Africa(Tigo) Moneyet Airtel Money).",
-          },
-        ],
-      },
+  },
+  {
+    theme: {
+      title: "Paiements",
+      content: [
+        {
+          question: "Quelles méthodes de paiements sont disponibles?",
+          anwser:
+            "Tous les paiements mobile money sont pris en charge (Moov Africa(Tigo) money et Airtel Money).",
+        },
+        {
+          question: "Les paiements fonctionnent-ils complètement hors ligne?",
+          anwser: "Nous n'avons pas encore vérifier.",
+        },
+        {
+          question: "Existe-t-il une politique de remboursement?",
+          anwser:
+            "Nous vous rembourserions 100% de votre argent si nous ne sommes pas en mesure de répondre à votre demande. Si vous souhaitez annuler la demande, nous ajouterons des frais de remboursement de 3% du montant.",
+        },
+      ],
     },
+  },
 ];
