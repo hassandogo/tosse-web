f24ed2c3abd4b150048abdbed3693906ad833a51
Hassan <hassanmahamatdogo@gmail.com>
2024-09-19 10:42:34 +0100

    Ajout de la page de terme et condition

---
+++ app/(app)/conditions/page.tsx
@@ -0,0 +1,284 @@
+import { AppContainer } from "@/components/global/appContainer";
+import Header from "@/components/global/header";
+import { Spacer } from "@/components/global/spacer";
+import { TypographyH1, TypographyP } from "@/components/ui/typographies";
+import { Space_Grotesk } from "next/font/google";
+import Link from "next/link";
+
+const Condition = () => {
+  return (
+    <section>
+      <Header />
+      <Spacer small />
+      <AppContainer>
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Termes et Conditions Générales
+            <div className="bg-primary h-1.5 w-[5rem] my-4"></div>
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Bienvenue chez TOSSE !<br />
+            Ces termes et conditions décrivent les règles et réglementations
+            relatives à l&apos;utilisation du site Web de Tosse Company Limited,
+            situé à l&apos;adresse https://www.”tosse”.net. En accédant à ce
+            site Web, nous supposons que vous acceptez ces termes et conditions.
+            N&apos;utilisez pas TOSSE si vous n&apos;acceptez pas tous les
+            termes et conditions énoncés sur cette page.
+            <br />
+            La terminologie suivante s&apos;applique aux présentes Conditions
+            générales, à la Déclaration de confidentialité et à l&apos;Avis de
+            non-responsabilité ainsi qu&apos;à tous les accords : « Client », «
+            Vous » et « Votre » font référence à vous, la personne qui se
+            connecte sur ce site Web et qui se conforme aux conditions générales
+            de la Société. « La Société », « Nous-mêmes », « Nous », « Notre »
+            et « Nos » font référence à notre Société. « Partie », « Parties »
+            ou « Nous » font référence à la fois au Client et à nous-mêmes. Tous
+            les termes font référence à l&apos;offre, à l&apos;acceptation et à
+            la prise en compte du paiement nécessaire pour entreprendre le
+            processus de notre assistance au Client de la manière la plus
+            appropriée dans le but exprès de répondre aux besoins du Client en
+            ce qui concerne la fourniture des services indiqués par la Société,
+            conformément et sous réserve de la loi en vigueur aux Pays-Bas.
+            Toute utilisation de la terminologie ci-dessus ou d&apos;autres mots
+            au singulier, au pluriel, en majuscules et/ou il/elle ou ils, sont
+            considérés comme interchangeables et donc comme faisant référence à
+            la même chose.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">Cookies</TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous utilisons des cookies. En accédant à Tosse, vous acceptez
+            d&apos;utiliser des cookies conformément à la politique de
+            confidentialité de “Tosse” Company Limited. La plupart des sites Web
+            interactifs utilisent des cookies pour nous permettre de récupérer
+            les informations de l&apos;utilisateur à chaque visite. Les cookies
+            sont utilisés par notre site Web pour activer la fonctionnalité de
+            certaines zones afin de faciliter la visite de notre site Web par
+            les personnes. Certains de nos partenaires affiliés/publicitaires
+            peuvent également utiliser des cookies.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">Licence</TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Sauf mention contraire, TOSSE Company Limited et/ou ses concédants
+            de licence détiennent les droits de propriété intellectuelle sur
+            tout le matériel de TOSSE. Tous les droits de propriété
+            intellectuelle sont réservés. Vous pouvez y accéder à partir de
+            TOSSE pour votre usage personnel, sous réserve des restrictions
+            définies dans les présentes conditions générales.
+          </TypographyP>
+          <Spacer tooSmall />
+          <ul>
+            Vous ne devez pas :
+            <Spacer tooSmall />
+            <li>- Rééditer le matériel de TOSSE</li>
+            <li>- Vendre, louer ou sous-licencier du matériel de TOSSE</li>
+            <li>- Reproduire, dupliquer ou copier le matériel de TOSSE</li>
+            <li>- Redistribuer le contenu de TOSSE</li>
+          </ul>
+          <Spacer tooSmall />
+          <TypographyP>
+            Le présent accord entrera en vigueur à la date des présentes.
+          </TypographyP>
+          <TypographyP>
+            Certaines parties de ce site Web offrent aux utilisateurs la
+            possibilité de publier et d&apos;échanger des opinions et des
+            informations dans certaines zones du site Web. TOSSE Company Limited
+            ne filtre pas, ne modifie pas, ne publie pas et ne révise pas les
+            commentaires avant leur présence sur le site Web. Les commentaires
+            ne reflètent pas les points de vue et les opinions de TOSSE Company
+            Limited, de ses agents et/ou de ses sociétés affiliées. Les
+            commentaires reflètent les points de vue et les opinions de la
+            personne qui publie ses points de vue et ses opinions. Dans la
+            mesure permise par les lois applicables, TOSSE Company Limited ne
+            sera pas responsable des commentaires ou de toute responsabilité,
+            dommages ou dépenses causés et/ou subis à la suite de toute
+            utilisation et/ou publication et/ou apparition des commentaires sur
+            ce site Web.
+          </TypographyP>
+          <TypographyP>
+            TOSSE Company Limited se réserve le droit de surveiller tous les
+            commentaires et de supprimer tout commentaire qui peut être
+            considéré comme inapproprié, offensant ou entraînant une violation
+            des présentes conditions générales.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Hyperliens vers notre contenu
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Les organisations suivantes peuvent créer un lien vers notre site
+            Web sans autorisation écrite préalable :
+          </TypographyP>
+          <Spacer tooSmall />
+          <ul>
+            Vous ne devez pas :
+            <Spacer tooSmall />
+            <li>- Organismes gouvernementaux;</li>
+            <li>- Moteurs de recherche;</li>
+            <li>- Organismes de presse;</li>
+            <li>
+              - Les distributeurs d&apos;annuaires en ligne peuvent créer un
+              lien vers notre site Web de la même manière qu&apos;ils créent un
+              lien hypertexte vers les sites Web d&apos;autres entreprises
+              répertoriées ; et
+            </li>
+            <li>
+              - Entreprises accréditées à l&apos;échelle du système, à
+              l&apos;exception des organisations à but non lucratif, des centres
+              commerciaux caritatifs et des groupes de collecte de fonds
+              caritatifs qui ne peuvent pas créer de lien hypertexte vers notre
+              site Web.
+            </li>
+          </ul>
+          <Spacer tooSmall />
+          <TypographyP>
+            Ces organisations peuvent créer des liens vers notre page
+            d&apos;accueil, vers des publications ou vers d&apos;autres
+            informations du site Web à condition que le lien : (a) ne soit en
+            aucun cas trompeur ; (b) n&apos;implique pas faussement un
+            parrainage, une approbation ou une approbation de la partie qui
+            établit le lien et de ses produits et/ou services ; et (c)
+            s&apos;inscrive dans le contexte du site de la partie qui établit le
+            lien.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">Cadres</TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Sans autorisation préalable et permission écrite, vous ne pouvez pas
+            créer de cadres autour de nos pages Web qui modifient de quelque
+            manière que ce soit la présentation visuelle ou l&apos;apparence de
+            notre site Web.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Responsabilité du contenu
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous ne pouvons être tenus responsables du contenu qui apparaît sur
+            votre site Web. Vous acceptez de nous protéger et de nous défendre
+            contre toutes les réclamations qui pourraient survenir sur votre
+            site Web. Aucun lien ne doit apparaître sur un site Web qui pourrait
+            être interprété comme diffamatoire, obscène ou criminel, ou qui
+            enfreint, viole ou préconise la violation ou toute autre violation
+            des droits d&apos;un tiers.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Votre confidentialité
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Veuillez lire{" "}
+            <Link href="/confiddentiality" className="text-primary">
+              la politique de confidentialité
+            </Link>
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">Réserve de droits</TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous nous réservons le droit de vous demander de supprimer tous les
+            liens ou tout lien particulier vers notre site Web. Vous acceptez de
+            supprimer immédiatement tous les liens vers notre site Web sur
+            demande. Nous nous réservons également le droit de modifier ces
+            termes et conditions et sa politique de liens à tout moment. En
+            créant en permanence un lien vers notre site Web, vous acceptez
+            d&apos;être lié et de suivre ces termes et conditions de liens.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Suppression des liens de notre site Web
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Si vous trouvez sur notre site Internet un lien offensant pour une
+            raison quelconque, vous êtes libre de nous contacter et de nous en
+            informer à tout moment. Nous étudierons les demandes de suppression
+            de liens, mais nous ne sommes pas obligés de le faire ou de vous
+            répondre directement.
+          </TypographyP>
+          <TypographyP>
+            Nous ne garantissons pas que les informations sur ce site Web sont
+            correctes, nous ne garantissons pas leur exhaustivité ou leur
+            exactitude ; nous ne promettons pas non plus de garantir que le site
+            Web reste disponible ou que le contenu du site Web est tenu à jour.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Clause de non-responsabilité
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Dans la mesure maximale permise par la loi applicable, nous excluons
+            toutes les représentations, garanties et conditions relatives à
+            notre site Web et à l&apos;utilisation de ce site Web. Rien dans
+            cette clause de non-responsabilité de :
+          </TypographyP>
+          <Spacer tooSmall />
+          <ul>
+            <li>
+              - limiter ou exclure notre ou votre responsabilité en cas de décès
+              ou de blessure corporelle ;
+            </li>
+            <li>
+              - limiter ou exclure notre ou votre responsabilité en cas de
+              fraude ou de fausse déclaration frauduleuse ;
+            </li>
+            <li>
+              - limiter l’une de nos ou vos responsabilités d’une manière qui
+              n’est pas autorisée par la loi applicable ; ou
+            </li>
+            <li>
+              - exclure l’une quelconque de nos responsabilités ou des vôtres
+              qui ne peuvent être exclues en vertu de la loi applicable.
+            </li>
+          </ul>
+          <Spacer tooSmall />
+          <TypographyP>
+            Les limitations et interdictions de responsabilité énoncées dans la
+            présente section et ailleurs dans la présente clause de
+            non-responsabilité :<br /> (a) sont soumises au paragraphe précédent
+            ; et
+            <br />
+            (b) régissent toutes les responsabilités découlant de la clause de
+            non-responsabilité, y compris les responsabilités contractuelles,
+            délictuelles et pour manquement à une obligation légale.
+          </TypographyP>
+          <TypographyP>
+            Tant que le site Web et les informations et services sur le site Web
+            sont fournis gratuitement, nous ne serons pas responsables de toute
+            perte ou dommage de quelque nature que ce soit.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+      </AppContainer>
+      <Spacer small />
+    </section>
+  );
+};
+export default Condition;
