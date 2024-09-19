96ac6065f118156e5169527c33611001b1b43fe5
Hassan <hassanmahamatdogo@gmail.com>
2024-09-19 09:13:39 +0100

    Ajout de la page de confidentialité

---
+++ app/(app)/confiddentiality/page.tsx
@@ -0,0 +1,323 @@
+import { AppContainer } from "@/components/global/appContainer";
+import Header from "@/components/global/header";
+import { Spacer } from "@/components/global/spacer";
+import { TypographyH1, TypographyP } from "@/components/ui/typographies";
+
+const Confidentiality = () => {
+  return (
+    <section>
+      <Header />
+      <Spacer small />
+      <AppContainer>
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Politique de confidentialité des données Tosse
+            <div className="bg-primary h-1.5 w-[5rem] my-4"></div>
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous prenons la confidentialité au sérieux. Cela met en évidence qui
+            nous sommes, quelles informations vous concernant nous collectons et
+            ce que nous en faisons. Les termes en majuscules qui ne sont pas
+            définis dans la présente Politique de confidentialité ont le sens
+            qui leur est donné dans nos Conditions d&apos;utilisation. Veuillez
+            également lire nos{" "}
+            <span className="text-primary">conditions d&apos;utilisation</span>{" "}
+            relatives au service qui vous intéresse. Elles fournissent plus
+            d&apos;informations sur la façon dont nous exerçons nos activités et
+            sur les restrictions d&apos;éligibilité qui peuvent
+            s&apos;appliquer. En utilisant notre service, vous consentez aux
+            pratiques en matière de données prescrites dans cette déclaration.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Quelles informations sont collectées ?
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous collectons uniquement des numéros de téléphone. Nous stockons
+            les transactions en utilisant le numéro de téléphone uniquement pour
+            identifier les utilisateurs. Nous pouvons également conserver tous
+            les messages que vous envoyez via le Service. En utilisant les
+            aspects payants du Service, vous pouvez être amené à partager des
+            informations de facturation et de paiement avec notre processeur de
+            paiement tiers, et vous acceptez expressément l&apos;utilisation de
+            vos informations de facturation et de paiement par ce processeur de
+            paiement tiers. Nous collectons et utilisons également des
+            informations sur vos interactions avec le Service d&apos;une manière
+            et dans un format qui ne permettent pas de vous identifier en tant
+            qu&apos;individu (« Informations non personnellement
+            identifiables »). Nous pouvons collecter, utiliser et divulguer les
+            types de données suivants
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Informations analytiques :
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous utilisons des outils d&apos;analyse tiers pour nous aider à
+            mesurer les tendances de trafic et d&apos;utilisation du Service.
+            Ces outils collectent les informations envoyées par votre navigateur
+            dans le cadre d&apos;une demande de page Web, y compris les pages
+            Web que vous visitez, les modules complémentaires de votre
+            navigateur, la largeur et la hauteur de votre navigateur, ainsi que
+            d&apos;autres informations qui nous aident à améliorer le Service.
+            Nous pouvons collecter et utiliser ces informations analytiques avec
+            vos informations personnelles identifiables pour créer un profil
+            plus large de nos utilisateurs individuels afin que nous puissions
+            mieux vous servir.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Informations sur les cookies :
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Lorsque vous visitez le Service, nous pouvons envoyer un ou
+            plusieurs cookies – un petit fichier texte contenant une chaîne de
+            caractères alphanumériques – à votre ordinateur qui identifie de
+            manière unique votre navigateur. Cela permet à Switchn de vous aider
+            à vous connecter plus rapidement et d&apos;améliorer votre
+            navigation sur le site. Un cookie peut également nous transmettre
+            des informations sur la manière dont vous utilisez le Service (par
+            exemple, les pages que vous consultez, les liens sur lesquels vous
+            cliquez et d&apos;autres actions que vous effectuez sur le Service),
+            et nous permettre, à nous ou à nos partenaires commerciaux, de
+            suivre votre utilisation du Service. au fil du temps. Un cookie
+            persistant reste sur votre disque dur après la fermeture de votre
+            navigateur. Des cookies persistants peuvent être utilisés par votre
+            navigateur lors de visites ultérieures sur le site. Les cookies
+            persistants peuvent être supprimés en suivant les instructions de
+            votre navigateur Web. Un cookie de session est temporaire et
+            disparaît après la fermeture de votre navigateur. Vous pouvez
+            réinitialiser votre navigateur Web pour refuser tous les cookies ou
+            pour indiquer quand un cookie est envoyé. Cependant, certaines
+            fonctionnalités du Serv&apos;ce peuvent ne pas fonctionner
+            correctement si la possibilité d&apos;accepter les cookies est
+            désactivée.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Effacer les informations sur les gifs/balises Web :
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Lorsque vous utilisez le Service, nous pouvons utiliser des gifs
+            clairs (également appelés balises Web) qui sont utilisés pour suivre
+            de manière anonyme les modèles d&apos;utilisation en ligne de nos
+            utilisateurs. En outre, nous pouvons également utiliser des gifs
+            clairs dans les e-mails HTML envoyés à nos utilisateurs pour suivre
+            quels e-mails sont ouverts et sur quels liens les destinataires
+            cliquent. Les informations permettent d’établir des rapports plus
+            précis et d’améliorer le Service.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Comment ces informations sont-elles utilisées?
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyH1 className="font-bold">
+            Informations personnellement identifiables :
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous pouvons utiliser les informations personnelles identifiables
+            que nous collectons ou recevons pour communiquer directement avec
+            vous. Nous vous enverrons occasionnellement des mises à jour par SMS
+            sur la technologie, annoncerons de nouvelles fonctionnalités, vous
+            rappellerons des événements ou vous informerons d&apos;autres
+            nouvelles importantes. Vous avez toujours la possibilité de vous
+            désinscrire de ces messages. Nous utilisons également vos
+            informations personnelles pour vous envoyer des e-mails liés au
+            service (par exemple, vérification du compte, modifications/mises à
+            jour des fonctionnalités du service, avis techniques et de
+            sécurité). Vous pouvez vous désinscrire des SMS liés au service.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Informations non personnellement identifiables:
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous utilisons ou pouvons utiliser des cookies, des fichiers
+            journaux et des informations gifs clairs pour: (a) mémoriser les
+            informations afin que vous n&apos;ayez pas à les saisir à nouveau
+            lors de votre visite ou la prochaine fois que vous visiterez le
+            site; (b) fournir du contenu et des informations personnalisés et
+            personnalisés; (c) pour fournir et surveiller l&apos;efficacité de
+            notre service; (d) surveiller les mesures globales telles que le
+            nombre total de visiteurs, le trafic et les tendances
+            démographiques; (e) diagnostiquer ou résoudre les problèmes
+            technologiques; (f) vous aider à &apos;ccéder efficacement à vos
+            informations; et (h) pour fournir de la publicité sur votre
+            appareil.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Les informations sont-elles partagées ou divulguées ?
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyH1 className="font-bold">
+            Informations personnellement identifiables :
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous ne vendrons aucune information personnellement identifiable à
+            des tiers en dehors de Switchn et de ses filiales et sociétés
+            affiliées contrôlées sans votre consentement, sauf dans les cas
+            indiqués ci-dessous:
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Avec qui pouvons-nous partager vos informations personnelles
+            identifiables:
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous pouvons partager vos informations personnelles avec des
+            partenaires commerciaux tiers dans le but de vous fournir le service
+            (par exemple, des fournisseurs de messagerie, des fournisseurs de
+            serveurs, etc.). Ces partenaires commerciaux auront un accès limité
+            aux informations personnelles identifiables qui sont raisonnablement
+            nécessaires à la fourniture du service, et nous exigerons que ces
+            tiers se conforment à la présente politique de confidentialité. Avec
+            qui vous pouvez choisir de partager vos informations personnelles
+            identifiables: toute information personnelle identifiable ou tout
+            contenu que vous divulguez volontairement pour publication sur le
+            service, tel que le contenu utilisateur, devient accessible au
+            public. Si vous supprimez les informations que vous avez publiées
+            sur le Service, des copies peuvent rester visibles dans les pages
+            mises en cache et archivées du Service, ou si d&apos;autres
+            utilisateurs ont copié ou enregistré ces informations. Nous faisons
+            appel à un fournisseur de services tiers pour gérer le traitement
+            des paiements. En utilisant les aspects payants du Service, vous
+            acceptez de partager vos informations de facturation et autres
+            informations de paiement avec tout processeur de paiement tiers dans
+            le but de traiter vos paiements. Informations non personnellement
+            identifiables: nous pouvons partager des informations non
+            personnellement identifiables, y compris des informations obtenues
+            via des outils tels que des cookies, des fichiers journaux et des
+            cadeaux clairs (tels que des données d&apos;utilisation anonymes,
+            des pages et URL de renvoi/sortie, des types de plateformes, le
+            nombre de clics). , etc.): (i) de toute manière autorisée pour les
+            informations personnelles identifiables; et (ii) avec nos
+            partenaires commerciaux tiers. Nous pouvons également regrouper ou
+            autrement supprimer les données de toutes les caractéristiques
+            d&apos;identification personnelle et pouvons partager ces données
+            agrégées et anonymisées avec des tiers. Accès Si vous souhaitez
+            corriger, modifier ou supprimer toute information présente sur le
+            site, vous pouvez nous envoyer un email à
+            mt&quot;tossemoro@gmail.com&quot;. Transferts internationaux Les
+            informations personnellement identifiables collectées via le Service
+            peuvent être stockées et traitées au Cameroun ou dans tout autre
+            pays dans lequel Tosse ou ses filiales, sociétés affiliées ou
+            prestataires de services possèdent des installations. Tosse peut
+            transférer les informations que nous collectons à votre sujet, y
+            compris les informations personnelles identifiables, à des entités
+            affiliées ou à d&apos;autres tiers au-delà des frontières et de
+            votre pays ou juridiction vers d&apos;autres pays ou juridictions
+            dans le monde. Si vous résidez aux États-Unis ou dans d&apos;autres
+            régions où les lois régissant la collecte et l&apos;utilisation des
+            données peuvent différer de la loi camerounaise, veuillez noter que
+            nous pouvons transférer des informations, y compris des informations
+            personnellement identifiables, vers un pays et une juridiction qui
+            n&apos;ont pas la même protection des données. lois en vigueur dans
+            votre juridiction, et vous consentez au transfert
+            d&apos;informations vers le Cameroun ou tout autre pays dans lequel
+            Tosse ou ses filiales, sociétés affiliées ou prestataires de
+            services maintiennent des installations et à l&apos;utilisation et à
+            la divulgation d&apos;informations vous concernant, y compris des
+            informations personnelles identifiables, comme décrit dans cette
+            politique de confidentialité. Enfants, Tosse ne collecte ni ne
+            sollicite sciemment d’informations personnelles identifiables auprès
+            de toute personne de moins de 18 ans et ne permet pas sciemment à
+            ces personnes de s’inscrire en tant qu’utilisateurs. Le Service et
+            son contenu ne sont pas destinés aux enfants de moins de 18 ans. Si
+            vous avez moins de 18 ans, veuillez ne nous envoyer aucune
+            information vous concernant, y compris votre nom, votre adresse,
+            votre numéro de téléphone ou votre adresse e-mail. Si nous apprenons
+            que nous avons collecté des informations personnelles sur un enfant
+            de moins de 18 ans sans vérification du consentement parental, nous
+            supprimerons ces informations le plus rapidement possible. Si vous
+            pensez que nous pourrions détenir des informations provenant ou
+            concernant un enfant de moins de 18 ans, veuillez nous contacter à
+            &quot;tossemoro@gmail.com&quot;.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Autres sites Web et services
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous ne sommes pas responsables des pratiques employées par les
+            sites Web ou services liés au Service, y compris les informations ou
+            le contenu qu&apos;ils contiennent. N&apos;oubliez pas que lorsque
+            vous utilisez un lien qui vous dirige de notre Service vers un autre
+            site Web, notre politique de confidentialité ne s&apos;applique pas
+            aux sites Web ou services tiers, y compris, sans limitation, aux
+            processeurs de paiement tiers. Votre navigation et vos interactions
+            sur tout site Web ou service tiers, y compris ceux qui ont un lien
+            sur notre site Web, sont soumises aux propres règles et politiques
+            de ce tiers. En outre, vous acceptez que nous ne soyons pas
+            responsables et que nous n&apos;exercions aucun contrôle sur les
+            tiers que vous autorisez à accéder à votre Contenu utilisateur. Si
+            vous utilisez un site Web ou un service tiers (comme Facebook) et
+            que vous autorisez un tel tiers à accéder à votre Contenu
+            utilisateur, vous le faites à vos propres risques. Cette politique
+            de confidentialité ne s&apos;applique pas aux informations que nous
+            collectons par d&apos;autres moyens (y compris hors ligne) et à
+            partir d&apos;autres sources que via le Service.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">
+            Modifications de notre politique de confidentialité
+          </TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Nous pouvons, à notre seule discrétion, modifier ou mettre à jour
+            cette politique de confidentialité de temps à autre, et vous devez
+            donc consulter cette page régulièrement. Lorsque nous modifions la
+            politique de manière significative, nous mettrons à jour la date de
+            « dernière modification » au bas de cette page.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+        <div className="space-4">
+          <TypographyH1 className="font-bold">Contacter Tosse</TypographyH1>
+          <Spacer tooSmall />
+          <TypographyP>
+            Si vous pensez que Tosse n&apos;a pas respecté cette politique de
+            confidentialité, veuillez nous contacter par e-mail à
+            &quot;tossemoro@gmail.com&quot; et nous déploierons des efforts commercialement
+            raisonnables pour remédier au problème. Cette politique de
+            confidentialité n&apos; a encore modifiée une fois.
+          </TypographyP>
+        </div>
+        <Spacer tooSmall />
+      </AppContainer>
+    </section>
+  );
+};
+
+export default Confidentiality;
--- app/(app)/faqs/page.tsx
+++ app/(app)/faqs/page.tsx
@@ -9,7 +9,8 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import Link from "next/link";
-import { TypographyP } from "@/components/ui/typographies";
+import { TypographyH1, TypographyH3, TypographyP } from "@/components/ui/typographies";
+
 const Faqs = () => {
   return (
     <section>
@@ -18,26 +19,26 @@ const Faqs = () => {
       <AppContainer>
         <div className="justify-center flex items-center flex-col gap-4">
           <div className="text-center space-y-2">
-            <h1 className="text-[3.125rem] font-bold leading-[3.75rem] text-[#1c2e3d] mb-[1.5rem]">
+            <TypographyH1>
               Questions fréquemment posées
-            </h1>
-            <p className="text-[2.5rem] font-normal leading-[3rem]">
+            </TypographyH1>
+            <TypographyP className="text-[2.5rem] font-normal leading-[3rem]">
               Nouveau sur Tosse ? Commençons par les bases.
-            </p>
+            </TypographyP>
           </div>
           {faqs &&
             faqs.length > 0 &&
-            faqs.map((faq) => (
-              <div className="w-[80%] space-y-3 py-4">
+            faqs.map((faq, index) => (
+              <div key={index} className="w-[80%] space-y-3 py-4">
                 <Spacer tooSmall />
-                <h3 className=" text-[2rem] font-normal  leading-[3.375rem] tracking-widest text-primary text-center">
+                <TypographyH3 className=" text-[2rem] text-primary text-center">
                   {faq.theme.title}
-                </h3>
+                </TypographyH3>
                 <div className="">
                   <Spacer small />
                   <Accordion type="single" collapsible className="">
                     {faq.theme.content.map((quest, index) => (
-                      <AccordionItem value={`item-${index}`}>
+                      <AccordionItem key={index} value={`item-${index}`}>
                         <AccordionTrigger className="text-2xl font-normal leading-9 text-left">
                           {quest.question}
                         </AccordionTrigger>
