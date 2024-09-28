98843b587a77860fbfb2cd8b8e4c65e2c88387fb
Hassan <hassanmahamatdogo@gmail.com>
2024-09-28 17:59:50 +0100

    J'ai modifier reach avec recherche...

--- app/(app)/(other)/forfaits/page.tsx
+++ app/(app)/(other)/forfaits/page.tsx
@@ -136,7 +136,7 @@ const Forfaits = () => {
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
               type="search"
-              placeholder="Search..."
+              placeholder="Recherche..."
               className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
               onChange={(event) => handleInputSearch(event.target.value)}
             />
